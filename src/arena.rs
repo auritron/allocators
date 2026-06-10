4f2f9f2760ba09df0e8b5931c03736b308e89d63
auritron <myself.anath@gmail.com>
2026-06-11 01:01:22 +0530

    offset in arena changed from usize -> Cell<usize>

--- src/arena.rs
+++ src/arena.rs
@@ -1,3 +1,5 @@
+use core::cell::Cell;
+
 const DEFAULT_CAPACITY: usize = 1024;
 
 #[derive(Debug)]
@@ -9,7 +11,7 @@ pub enum ArenaError {
 #[repr(align(8))]
 pub struct Arena<const N: usize = DEFAULT_CAPACITY> {
     container: [u8; N],
-    offset: usize,
+    offset: Cell<usize>,
 }
 
 // constructors and accessors
@@ -19,47 +21,49 @@ impl<const N: usize> Arena<N> {
 
         Self {
             container: [0; N],
-            offset: 0,
+            offset: Cell::new(0),
         } 
 
     }
 
     pub fn capacity(&self) -> usize { N }
-    pub fn allocated_bytes(&self) -> usize { self.offset }
+    pub fn allocated_bytes(&self) -> usize { self.offset.get() }
 
 }
 
 // direct allocation
 impl<const N: usize> Arena<N> {
 
-    pub fn alloc_bytes<'a>(&'a mut self, size: usize) -> Result<&'a mut [u8], ArenaError> {
+    pub fn alloc_bytes<'a>(&'a self, size: usize) -> Result<&'a mut [u8], ArenaError> {
         unsafe {
-            if (self.offset + size) <= N {
-                let start_ptr: *mut u8 = self.container.as_mut_ptr();
-                self.offset += size;
-                Ok(core::slice::from_raw_parts_mut(start_ptr.add(self.offset - size), size))
+            let new_offset = self.offset.get() + size;
+            if (new_offset) <= N {
+                let start_ptr: *mut u8 = self.container.as_ptr() as *mut u8;
+                self.offset.set(new_offset);
+                Ok(core::slice::from_raw_parts_mut(start_ptr.add(new_offset - size), size))
             } else {
                 Err(ArenaError::AllocationError)
             }
         }
     }
 
-    pub fn alloc_align<'a>(&'a mut self, size: usize, align: usize) -> Result<&'a mut [u8], ArenaError> {
+    pub fn alloc_align<'a>(&'a self, size: usize, align: usize) -> Result<&'a mut [u8], ArenaError> {
 
         if align == 0 || (align & (align - 1)) != 0 { return Err(ArenaError::AlignmentError);}
 
-        let cur_addr = unsafe { self.container.as_mut_ptr().add(self.offset) } as usize;
+        let cur_addr = unsafe { (self.container.as_ptr() as *mut u8).add(self.offset.get()) } as usize;
         let align_addr = (cur_addr + align - 1) & !(align - 1);
         let padding = align_addr - cur_addr;
         
-        if self.offset + padding + size > N { return Err(ArenaError::AllocationError); }
-        self.offset += padding;
+        let new_offset = self.offset.get() + padding;
+        if new_offset + size > N { return Err(ArenaError::AllocationError); }
+        self.offset.set(new_offset);
         self.alloc_bytes(size)
 
     }
 
     pub fn reset(&mut self) -> () {
-        self.offset = 0;
+        self.offset.set(0);
     }
 
 }
